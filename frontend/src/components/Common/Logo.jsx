3d771dbcff7842b1f408176a8abacddd35e8743b
Paula <Paula.Castillejo1@alu.uclm.es>
2026-01-05 17:51:18 +0100

    v0.15.1 2026-01-05 | Adjustments to the spacing between elements in the responsive header on mobile devices.

--- frontend/src/components/Common/Logo.jsx
+++ frontend/src/components/Common/Logo.jsx
@@ -1,6 +1,6 @@
 const Logo = () => {
     return(
-        <div className="flex items-center gap-3" >
+        <div className="flex items-center gap-1 sm:gap-3" >
             <img src="/LogoClicklass.png" alt="Clicklass Logo" className={`h-5 sm:h-10 w-auto `} />
             <span className="text-xl sm:text-3xl text-red-700 font-bold">Clicklass</span>
         </div>

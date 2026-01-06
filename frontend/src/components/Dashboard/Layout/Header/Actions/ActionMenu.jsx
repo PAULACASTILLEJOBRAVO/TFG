daaf2e2182a6f1745c3641a49805dc109a9206e1
Paula <Paula.Castillejo1@alu.uclm.es>
2026-01-06 20:43:44 +0100

    v0.16.1 2026-01-06 | Fix action buttons for teacher and admin.

--- frontend/src/components/Dashboard/Layout/Header/Actions/ActionMenu.jsx
+++ frontend/src/components/Dashboard/Layout/Header/Actions/ActionMenu.jsx
@@ -10,7 +10,7 @@ import { Button } from "@/components/ui/button";
 import { useAuth } from "@/auth/AuthContext";
 
 const ActionMenu = ({icon}) => {
-    const user = useAuth();
+    const { user } = useAuth();
 
     if(!user) return null;
 
